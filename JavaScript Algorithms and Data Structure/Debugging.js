c2cf45186d9040f13a525ec499214650624b0eb2
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-08-08 10:19:39 -0400

    debugging exercises

---
+++ JavaScript Algorithms and Data Structure/Debugging.js
@@ -0,0 +1,53 @@
+// Use the JavaScript Console to Check the Value of a Variable
+  let a = 5;
+  let b = 1;
+  a++;
+  // Only change code below this line
+
+  let sumAB = a + b;
+  console.log(a);
+
+// Understanding the Differences between the freeCodeCamp and Browser Console
+  // Open your browser console.
+  let output = "Get this to log once in the freeCodeCamp console and twice in the browser console";
+  // Use console.log() to print the output variable.
+  console.clear()
+  console.log(output)
+  // Run the tests to see the difference between the two consoles.
+  // Now, add console.clear() before your console.log() to clear the browser console, and pass the tests.
+
+// Use typeof to Check the Type of a Variable
+  let seven = 7;
+  let three = "3";
+  console.log(seven + three);
+  // Only change code below this line
+  console.log(typeof seven);
+  console.log(typeof three);
+
+// Catch Misspelled Variable and Function Names
+  let receivables = 10;
+  let payables = 8;
+  let netWorkingCapital = receivables - payables;
+  console.log(`Net working capital is: ${netWorkingCapital}`);
+
+// Catch Unclosed Parentheses, Brackets, Braces and Quotes
+  let myArray = [1, 2, 3];
+  let arraySum = myArray.reduce((previous, current) =>  previous + current);
+  console.log(`Sum of array values is: ${arraySum}`);
+
+// Catch Mixed Usage of Single and Double Quotes
+  let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+  console.log(innerHtml);
+
+// Catch Use of Assignment Operator Instead of Equality Operator
+  let x = 7;
+  let y = 9;
+  let result = "to come";
+
+  if(x === y) {
+    result = "Equal!";
+  } else {
+    result = "Not equal!";
+  }
+
+  console.log(result);
