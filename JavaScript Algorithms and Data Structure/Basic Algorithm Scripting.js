127f5e4acd24c52263fcfa7181c7e063d297d56a
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-08-12 18:40:55 -0400

    basic algorithm exercises

--- JavaScript Algorithms and Data Structure/Basic Algorithm Scripting.js
+++ JavaScript Algorithms and Data Structure/Basic Algorithm Scripting.js
@@ -25,3 +25,27 @@
   factorialize(5);
 
 // Find the Longest Word in a String
+  function findLongestWordLength(str) {
+    let answer = 0;
+    str.split(' ').forEach(word => {
+      if (word.length >= answer) {
+        answer = word.length
+      }
+    });
+    return answer;
+  }
+
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+// Return Largest Numbers in Arrays
+  function largestOfFour(arr) {
+    let newArr = [];
+    arr.forEach(secondArr => {
+      newArr.push(Math.max(...secondArr))
+    })
+    return newArr
+  }
+
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// Confirm the Ending
