7d4d6221627feaa93bbdeec18217864a0b080d09
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-12 13:34:45 -0400

    object exercise

--- JavaScript Algorithms and Data Structure/format.js
+++ JavaScript Algorithms and Data Structure/format.js
@@ -1,11 +1,37 @@
-function addTogether() {
-  let args = Array.from(arguments);
-  console.log(args);
-  return args.some((arg) => typeof arg !== "number")
-    ? undefined
-    : args.length > 1
-    ? args[0] + args[1]
-    : (n) => (typeof n !== "number" ? undefined : n + args[0]);
-}
+var Person = function (firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  this.getFullName = function () {
+    return this.fullName
+      ? this.fullName
+      : firstAndLast.split(" ")[0] + " " + firstAndLast.split(" ")[1];
+  };
+  this.getFirstName = function () {
+    return this.firstName ? this.firstName : firstAndLast.split(" ")[0];
+  };
+  this.getLastName = function () {
+    return this.lastName ? this.lastName : firstAndLast.split(" ")[1];
+  };
+  this.setFirstName = function (first) {
+    this.firstName = first;
+    this.lastName = this.lastName ? this.lastName : firstAndLast.split(" ")[1];
+    this.fullName = this.firstName + " " + this.lastName;
+  };
+  this.setLastName = function (last) {
+    this.lastName = last;
+    this.firstName = this.firstName
+      ? this.firstName
+      : firstAndLast.split(" ")[0];
 
-addTogether(2, 3);
+    this.fullName = this.firstName + " " + this.lastName;
+  };
+  this.setFullName = function (firstAndLast) {
+    this.firstName = firstAndLast.split(" ")[0];
+    this.lastName = firstAndLast.split(" ")[1];
+    this.fullName = this.firstName + " " + this.lastName;
+  };
+  return firstAndLast;
+};
+
+var bob = new Person("Bob Ross");
+bob.getFullName();
