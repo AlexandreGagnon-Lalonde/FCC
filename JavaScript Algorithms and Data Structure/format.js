a85f2d7d5817ae14b2db31170a445b772046d96e
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-20 11:56:34 -0400

    cash register

--- JavaScript Algorithms and Data Structure/format.js
+++ JavaScript Algorithms and Data Structure/format.js
@@ -1,7 +1,74 @@
-function telephoneCheck(str) {
-  return str.match(/^(1|)(-| |)(\(\d{3}\)|\d{3})(-| |)\d{3}(-| |)\d{4}$/g)
-    ? true
-    : false;
+function checkCashRegister(price, cash, cid) {
+  let dues = cash - price;
+  let amountInDrawer = 0;
+  cid.forEach((arr) => {
+    amountInDrawer += arr[1];
+  });
+  if (
+    price > Number(amountInDrawer.toFixed(2)) &&
+    cash - price !== Number(amountInDrawer.toFixed(2))
+  ) {
+    return {
+      status: "INSUFFICIENT_FUNDS",
+      change: [],
+    };
+  } else if (cash - price === Number(amountInDrawer.toFixed(2))) {
+    return {
+      status: "CLOSED",
+      change: cid,
+    };
+  } else {
+    let change = [];
+    cid
+      .map((arr) => {
+        switch (arr[0]) {
+          case "PENNY":
+            return ["PENNY", 0.01, arr[1]];
+          case "NICKEL":
+            return ["NICKEL", 0.05, arr[1]];
+          case "DIME":
+            return ["DIME", 0.1, arr[1]];
+          case "QUARTER":
+            return ["QUARTER", 0.25, arr[1]];
+          case "ONE":
+            return ["ONE", 1, arr[1]];
+          case "FIVE":
+            return ["FIVE", 5, arr[1]];
+          case "TEN":
+            return ["TEN", 10, arr[1]];
+          case "TWENTY":
+            return ["TWENTY", 20, arr[1]];
+          case "ONE HUNDRED":
+            return ["ONE HUNDRED", 100, arr[1]];
+        }
+      })
+      .reverse()
+      .forEach((arr) => {
+        let index = 0;
+        while (Number(dues.toFixed(2)) - arr[1] >= 0 && arr[2] > 0) {
+          dues -= arr[1];
+          arr[2] -= arr[1];
+          index++;
+        }
+        if (index > 0) {
+          change.push([arr[0], index * arr[1]]);
+        }
+      });
+    return {
+      status: "OPEN",
+      change: change,
+    };
+  }
 }
 
-telephoneCheck("555-555-5555");
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
