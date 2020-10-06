1896b9cd432fa3233fd59c62563f88d576fb464c
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-06 18:51:41 -0400

    state and methods

--- Front End Libraries/React.js
+++ Front End Libraries/React.js
@@ -588,3 +588,79 @@
   }
 
 // Create a Controlled Input
+  class ControlledInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: "",
+      };
+      // change code below this line
+      this.handleChange = this.handleChange.bind(this);
+      // change code above this line
+    }
+    // change code below this line
+    handleChange(event) {
+      this.setState({
+        input: event.target.value,
+      });
+    }
+    // change code above this line
+    render() {
+      return (
+        <div>
+          {/* change code below this line */}
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.handleChange.bind(this)}
+          />
+          {/* change code above this line */}
+          <h4>Controlled Input:</h4>
+          <p>{this.state.input}</p>
+        </div>
+      );
+    }
+  }
+
+// Create a Controlled Form
+  class MyForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: "",
+        submit: "",
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value,
+      });
+    }
+    handleSubmit(event) {
+      // change code below this line
+      event.preventDefault();
+      this.setState({
+        submit: this.state.input,
+      });
+      // change code above this line
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            {/* change code below this line */}
+            <input value={this.state.input} onChange={this.handleChange} />
+            {/* change code above this line */}
+            <button type="submit">Submit!</button>
+          </form>
+          {/* change code below this line */}
+          <h1>{this.state.submit}</h1>
+          {/* change code above this line */}
+        </div>
+      );
+    }
+  }
+
+// Pass State as Props to Child Components
--- Front End Libraries/format.js
+++ Front End Libraries/format.js
@@ -1,46 +1,38 @@
-class Counter extends React.Component {
+class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      input: "",
+      submit: "",
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+  handleSubmit(event) {
     // change code below this line
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input,
+    });
     // change code above this line
   }
-  // change code below this line
-  increment() {
-    this.setState((state) => {
-      console.log(state);
-      return { count: state.count + 1 };
-    });
-  }
-  decrement() {
-    this.setState((state) => {
-      return { count: state.count - 1 };
-    });
-  }
-  reset() {
-    this.setState((state) => {
-      return { count: 0 };
-    });
-  }
-  // change code above this line
   render() {
     return (
       <div>
-        <button className="inc" onClick={this.increment}>
-          Increment!
-        </button>
-        <button className="dec" onClick={this.decrement}>
-          Decrement!
-        </button>
-        <button className="reset" onClick={this.reset}>
-          Reset
-        </button>
-        <h1>Current Count: {this.state.count}</h1>
+        <form onSubmit={this.handleSubmit}>
+          {/* change code below this line */}
+          <input value={this.state.input} onChange={this.handleChange} />
+          {/* change code above this line */}
+          <button type="submit">Submit!</button>
+        </form>
+        {/* change code below this line */}
+        <h1>{this.state.submit}</h1>
+        {/* change code above this line */}
       </div>
     );
   }
