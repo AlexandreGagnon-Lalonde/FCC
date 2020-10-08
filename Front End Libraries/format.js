30ff70e70dbd05e126e285a1904985276d853e4a
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-10-07 22:04:17 -0400

    props

--- Front End Libraries/format.js
+++ Front End Libraries/format.js
@@ -1,38 +1,27 @@
-class MyForm extends React.Component {
+class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "",
-      submit: "",
+      name: "CamperBot",
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      input: event.target.value,
-    });
-  }
-  handleSubmit(event) {
-    // change code below this line
-    event.preventDefault();
-    this.setState({
-      submit: this.state.input,
-    });
-    // change code above this line
   }
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          {/* change code below this line */}
-          <input value={this.state.input} onChange={this.handleChange} />
-          {/* change code above this line */}
-          <button type="submit">Submit!</button>
-        </form>
-        {/* change code below this line */}
-        <h1>{this.state.submit}</h1>
-        {/* change code above this line */}
+        <Navbar name={this.state.name} />
+      </div>
+    );
+  }
+}
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, my name is: {this.props.name} </h1>
       </div>
     );
   }
